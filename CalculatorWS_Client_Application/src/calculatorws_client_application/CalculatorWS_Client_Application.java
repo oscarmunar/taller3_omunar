eeac5fc98571f5ec5b033fce8a5ceb7768fb1a2c
oscarmunar <oscar.munar.suarez@gmail.com>
2017-10-15 09:38:23 -0500

    Add files via upload

---
+++ CalculatorWS_Client_Application/src/calculatorws_client_application/CalculatorWS_Client_Application.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorws_client_application;
+
+/**
+ *
+ * @author Oscar
+ */
+public class CalculatorWS_Client_Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
